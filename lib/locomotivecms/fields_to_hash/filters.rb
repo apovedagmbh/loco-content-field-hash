949fa39897f0cd065c2658ab9b830377328e6451
Tobias Geisler <t.geisler@apoveda.ch>
2018-02-09 16:54:21 +0100

    basic filter implementation

--- lib/locomotivecms/fields_to_hash/filters.rb
+++ lib/locomotivecms/fields_to_hash/filters.rb
@@ -1,8 +1,23 @@
 module Locomotivecms
   module FieldsToHash
     module Filters
-      def fields_to_hash
+      def fields_to_hash(fields, options)
+        first = options[:first].to_sym
+        second = options[:second].to_sym
 
+        first_keys = fields.keys.grep /^#{first}_\d*$/
+        second_keys = fields.keys.grep /^#{second}_\d*$/
+
+        hash_array = []
+
+        for i in 0..([first_keys.length, second_keys.length].max - 1)
+          hash = Hash.new
+          hash[first] = fields[first_keys[i]]
+          hash[second] = fields[second_keys[i]]
+          hash_array << hash
+        end
+
+        return hash_array;
       end
     end
   end
--- spec/locomotivecms/fields_to_hash_spec.rb
+++ spec/locomotivecms/fields_to_hash_spec.rb
@@ -6,24 +6,22 @@ RSpec.describe Locomotivecms::FieldsToHash::Filters do
       field_desc_1: 'Lorem ipsum 1',
       field_2: 'Field 2',
       field_desc_2: 'Lorem ipsum 2',
-      field_3: 'Field 3',
-      field_desc_3: 'Lorem ipsum 3',
-      field_4: 'Field 4',
-      field_desc_4: 'Lorem ipsum 4',
-      field_5: 'Field 5',
-      field_desc_5: 'Lorem ipsum 5',
+      field_10: 'Field 10',
+      field_desc_10: 'Lorem ipsum 10',
+      other_field_1: 'Other Field',
+      other_field_desc_1: 'Why am I here?',
+      field_1_other: 'Shouldn\'t match',
+      field_desc_1_other: 'Yea, you messed up'
     }
 
     it "creates a hash from a content entry's field" do
       hash_array = [
         { field: 'Field 1', field_desc: 'Lorem ipsum 1' },
         { field: 'Field 2', field_desc: 'Lorem ipsum 2' },
-        { field: 'Field 3', field_desc: 'Lorem ipsum 3' },
-        { field: 'Field 4', field_desc: 'Lorem ipsum 4' },
-        { field: 'Field 5', field_desc: 'Lorem ipsum 5' },
+        { field: 'Field 10', field_desc: 'Lorem ipsum 10' },
       ]
 
-      fields_to_hash(content_entry, { first: 'field', second: 'field_desc' }).should eq(hash_array)
+      expect(fields_to_hash(content_entry, { first: 'field', second: 'field_desc' })).to match_array(hash_array)
     end
   end
 end
